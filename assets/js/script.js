ea40281d29b9a16d807545cb3b74708c07f5ed02
DanilleH22 <>
2023-10-09 17:06:53 +0000

    add(script.js): comments for function and codes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,13 @@
 let score = 0;
 let timer = 30;
 
+// variables linking code index.html to script.js
 let timerId = document.getElementById("timer");
 let scoreId = document.getElementById("score");
 let startButton = document.getElementById("start");
 let bart = document.getElementById("face");
 let gameContainer = document.querySelector(".gamearea");
+let alertContainer = document.getElementById("instructionContainer");
 
 let contHeight;
 let contWidth;
@@ -13,17 +15,12 @@ let intervalId;
 
 bart.style.display = "none";
 
+// event listener to start game and show Bart's face
 startButton.addEventListener('click', () => {
     bart.style.display = 'block';
     startGame();
 
 });
-
-/**
- * Funtion for instructions pop up
- */
-
-let alertContainer = document.getElementById("instructionContainer");
 
 function instructions() {
     alertContainer.style.display = "block";
@@ -34,7 +31,8 @@ function hide() {
 }
 
 /** Level one start of the game */
-
+// function that allows the timer to begin, move Bart's face randomly over screen,
+// increase points for clicking Bart's face and reset game when timer is finished 
 function startGame() {
 
 
@@ -63,6 +61,7 @@ function startGame() {
 /**
  * Increase the score 
  */
+// Increase the score everytime Bart's face is clicked
 function incrementScore() {
     if (timer > 0) {
         score++;
@@ -71,7 +70,7 @@ function incrementScore() {
 }
 
 /** Timer */
-
+// Timer is to go down when started, when it hit's 0 an alert message appears
 function gameTimer() {
     timer--;
     timerId.textContent = timer + ' seconds';
@@ -84,7 +83,7 @@ function gameTimer() {
 
 
 /** Reset game */
-
+// when the game is finished it resets variables
 function reset() {
     score = 0;
     timer = 30;
